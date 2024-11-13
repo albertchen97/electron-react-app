ef08d67ca9c273b3fb6d102b529a5fe624e8de70
Haoze Chen <albertchen2017@outlook.com>
2024-11-13 13:52:36 +0800

    set up electron

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Change the default output directory name from "dist" to "dist-react" to avoid naming conflict with Electron
-    outDir: "dist-react",
-  },
+	plugins: [react()],
+	base: "./",
+	build: {
+		// Change the default output directory name from "dist" to "dist-react" to avoid naming conflict with Electron
+		outDir: "dist-react",
+	},
 });
