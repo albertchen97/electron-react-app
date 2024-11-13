8e91093a642f01d1c20733425cc51cc4c8a336ad
Haoze Chen <albertchen2017@outlook.com>
2024-11-13 15:11:31 +0800

    set up electron builder

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-type test = string;
-
 app.on("ready", () => {
     // Create a main browswer window when the app is ready
     const mainWindow = new BrowserWindow({});
