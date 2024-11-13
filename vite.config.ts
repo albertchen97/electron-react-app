b00aad20fdc79811fccce48fae3a8b2b81beca02
Haoze Chen <albertchen2017@outlook.com>
2024-11-13 12:29:53 +0800

    set up react-ts boilerplace; set up React

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Change the default output directory name from "dist" to "dist-react" to avoid naming conflict with Electron
+    outDir: "dist-react",
+  },
+});
