96625906f052e889a5eff834326e28cb8d3c2a14
Haoze Chen <albertchen2017@outlook.com>
2024-11-13 15:47:26 +0800

    (improve DX) load the frontend from Vite server with HMR when in dev mode

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,10 +1,18 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
     // Create a main browswer window when the app is ready
     const mainWindow = new BrowserWindow({});
 
-    // Load the index.html page
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+    if (isDev()) {
+        // frontend DX improvement: load the Vite server HMR-hosted frontend part
+        mainWindow.loadURL("http://localhost:5123");
+    } else {
+        // load from the distribution directory
+        mainWindow.loadFile(
+            path.join(app.getAppPath(), "/dist-react/index.html")
+        );
+    }
 });
